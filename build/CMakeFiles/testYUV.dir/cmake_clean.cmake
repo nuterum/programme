40add44ae0ca56b29b67da2a8618eb65b4a3d069
nuterum <ludoviccamelin@hotmail.fr>
2013-06-28 13:50:56 +0200

    Save2

---
+++ build/CMakeFiles/testYUV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testYUV.dir/src/test_video_YUV420.c.o"
+  "testYUV.pdb"
+  "testYUV"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/testYUV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/test_drone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_drone.dir/src/test_drone.c.o"
+  "test_drone.pdb"
+  "test_drone"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/test_drone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
